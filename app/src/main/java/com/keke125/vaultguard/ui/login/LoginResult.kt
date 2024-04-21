59cf0826292700f41da40288de13fdb1481e9cae
keke125 <admin@keke125.com>
2024-04-21 17:09:48 +0800

    Chore: Removed unused code.

--- app/src/main/java/com/keke125/vaultguard/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.keke125.vaultguard.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
