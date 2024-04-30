3df5f649aaebea6d32000caa8173af49ec9862f4
keke125 <admin@keke125.com>
2024-04-30 21:49:05 +0800

    Feat: Introduce Vault Dialog UI Screen with Copy Functionality.

--- app/src/main/java/com/keke125/vaultguard/screen/VaultScreen.kt
+++ app/src/main/java/com/keke125/vaultguard/screen/VaultScreen.kt
@@ -1,7 +1,10 @@
 package com.keke125.vaultguard.screen
 
 import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
+import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -13,19 +16,29 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.ContentCopy
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Visibility
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.asLiveData
 import androidx.navigation.NavController
+import com.keke125.vaultguard.data.Vault
 import com.keke125.vaultguard.ui.theme.VaultGuardTheme
 import com.keke125.vaultguard.util.DatabaseUtil
 
@@ -36,8 +49,10 @@ fun VaultScreen(navController: NavController) {
         Surface(
             modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
         ) {
-            val vaults =  DatabaseUtil.getAllVaults().asLiveData()
+            val vaults = DatabaseUtil.getAllVaults().asLiveData()
             val vaultsState = vaults.observeAsState(emptyList())
+            val context = navController.context
+            val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
             Column {
                 Text(
                     "Vault Screen",
@@ -49,8 +64,10 @@ fun VaultScreen(navController: NavController) {
                         modifier = Modifier.fillMaxWidth()
                     ) {
                         items(vaultsState.value) { vault ->
-                            ListItem(
-                                headlineContent = { Text(vault.name) },
+                            val (expanded, onExpandedChange) = remember {
+                                mutableStateOf(false)
+                            }
+                            ListItem(headlineContent = { Text(vault.name) },
                                 supportingContent = { Text(vault.username) },
                                 leadingContent = {
                                     Icon(
@@ -59,15 +76,18 @@ fun VaultScreen(navController: NavController) {
                                     )
                                 },
                                 trailingContent = {
-                                    IconButton(onClick = { /*TODO*/ }) {
-                                        Icon(imageVector = Icons.Default.MoreVert, contentDescription = "")
+                                    IconButton(onClick = { onExpandedChange(true) }) {
+                                        Icon(
+                                            imageVector = Icons.Default.MoreVert,
+                                            contentDescription = ""
+                                        )
                                     }
                                 },
-                                modifier = Modifier.clickable{
+                                modifier = Modifier.clickable {
 
-                                }
-                            )
+                                })
                             HorizontalDivider()
+                            VaultDialog(expanded, onExpandedChange, vault, clipboard, context)
                         }
                     }
                 }
@@ -97,4 +117,87 @@ fun checkPassword(
     } else {
         return true
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun VaultDialog(
+    expanded: Boolean,
+    onExpandedChange: (Boolean) -> Unit,
+    vault: Vault,
+    clipboardManager: ClipboardManager,
+    context: Context
+) {
+    when {
+        expanded -> {
+            BasicAlertDialog(onDismissRequest = { onExpandedChange(false) }) {
+                Column {
+                    ListItem(headlineContent = { Text(vault.name) })
+                    ListItem(headlineContent = { Text("View") }, leadingContent = {
+                        Icon(
+                            Icons.Outlined.Visibility, contentDescription = ""
+                        )
+                    })
+                    ListItem(headlineContent = { Text("Edit") }, leadingContent = {
+                        Icon(
+                            Icons.Outlined.Edit, contentDescription = ""
+                        )
+                    })
+                    ListItem(headlineContent = { Text("Copy Username") }, leadingContent = {
+                        Icon(
+                            Icons.Outlined.ContentCopy, contentDescription = ""
+                        )
+                    }, modifier = Modifier.clickable {
+                        copyUsername(
+                            clipboardManager, vault.username, context
+                        )
+                    })
+                    ListItem(headlineContent = { Text("Copy Password") }, leadingContent = {
+                        Icon(
+                            Icons.Outlined.ContentCopy, contentDescription = ""
+                        )
+                    }, modifier = Modifier.clickable {
+                        copyPassword(
+                            clipboardManager, vault.password, context
+                        )
+                    })
+                    ListItem(headlineContent = { }, trailingContent = {
+                        TextButton(onClick = { onExpandedChange(false) }) {
+                            Text("Cancel", color = ListItemDefaults.colors().headlineColor)
+                        }
+                    })
+                }
+            }
+        }
+    }
+}
+
+fun copyUsername(clipboardManager: ClipboardManager, username: String, context: Context) {
+    // When setting the clipboard text.
+    clipboardManager.setPrimaryClip(ClipData.newPlainText("username", username))
+    // Only show a toast for Android 12 and lower.
+    //if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2)
+    Toast.makeText(
+        context,
+        "Copied",
+        Toast.LENGTH_SHORT
+    ).show()
+}
+
+fun copyPassword(clipboardManager: ClipboardManager, password: String, context: Context) {
+    val clipData = ClipData.newPlainText("password", password)
+    clipData.apply {
+        description.extras = PersistableBundle().apply {
+            putBoolean("android.content.extra.IS_SENSITIVE", true)
+        }
+    }
+    // When setting the clipboard text.
+    clipboardManager.setPrimaryClip(clipData)
+    // Only show a toast for Android 12 and lower.
+    // if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2)
+    Toast.makeText(
+        context,
+        "Copied",
+        Toast.LENGTH_SHORT
+    ).show()
 }
