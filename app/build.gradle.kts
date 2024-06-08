104aaa48cab9c778e20579b3dd0977b707969ae6
keke125 <admin@keke125.com>
2024-06-09 01:43:36 +0800

    Chore: Update version to 1.7.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.keke125.vaultguard"
         minSdk = 24
         targetSdk = 34
-        versionCode = 20
-        versionName = "1.7.1"
+        versionCode = 21
+        versionName = "1.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
