ef7d73fa97a75aa8356e61bb9ee5e4e65316d755
keke125 <admin@keke125.com>
2024-06-09 20:27:49 +0800

    Feat: Add notification dialog to inform users that exported vaults will not be encrypted before export.

--- app/src/main/java/com/keke125/vaultguard/activity/ExportVaultActivity.kt
+++ app/src/main/java/com/keke125/vaultguard/activity/ExportVaultActivity.kt
@@ -19,6 +19,8 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Password
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -28,6 +30,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -80,9 +83,15 @@ fun ExportVaultScreen(
     val vaultUiState by viewModel.vaultUiState.collectAsState()
     val folderUiState by viewModel.folderUiState.collectAsState()
     val activity = LocalContext.current as? Activity
-    val jsonString = viewModel.exportVaultAndFolder(vaultUiState.vaultList, folderUiState.folderList)
+    val jsonString =
+        viewModel.exportVaultAndFolder(vaultUiState.vaultList, folderUiState.folderList)
     val contentResolver = context.contentResolver
     val (isPasswordVisible, onPasswordVisibleChange) = remember { mutableStateOf(false) }
+    val (isExportVaultsConfirmExpanded, onExportVaultsConfirmExpandedChange) = remember {
+        mutableStateOf(
+            false
+        )
+    }
     val createFileResultLauncher =
         rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
@@ -134,7 +143,8 @@ fun ExportVaultScreen(
                 .padding(vertical = 8.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            OutlinedTextField(value = viewModel.exportVaultsUiState.exportVaults.password,
+            OutlinedTextField(
+                value = viewModel.exportVaultsUiState.exportVaults.password,
                 onValueChange = {
                     viewModel.updateUiState(
                         viewModel.exportVaultsUiState.exportVaults.copy(password = it)
@@ -167,36 +177,73 @@ fun ExportVaultScreen(
                     return@Button
                 }
                 if (viewModel.checkMainPassword(viewModel.exportVaultsUiState.exportVaults.password)) {
-                    if (vaultUiState.vaultList.isNotEmpty() || folderUiState.folderList.isNotEmpty()) {
-                        val timeStamp: String
-                        if (Build.VERSION.SDK_INT >= 26) {
-                            val formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
-                            timeStamp = LocalDateTime.now().format(formatter).toString()
-                        } else {
-                            val simpleDateFormat =
-                                SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault())
-                            timeStamp = simpleDateFormat.format(Date())
-                        }
-                        val createFileIntent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
-                            addCategory(Intent.CATEGORY_OPENABLE)
-                            type = "application/json"
-                            putExtra(Intent.EXTRA_TITLE, "vaultguard-$timeStamp-export.json")
-                        }
-                        viewModel.updateUiState(
-                            viewModel.exportVaultsUiState.exportVaults.copy(
-                                password = ""
-                            )
-                        )
-                        createFileResultLauncher.launch(createFileIntent)
-                    } else {
-                        Toast.makeText(context, "尚未儲存密碼或資料夾!", Toast.LENGTH_SHORT).show()
-                    }
+                    onExportVaultsConfirmExpandedChange(true)
                 } else {
                     Toast.makeText(context, "主密碼錯誤!", Toast.LENGTH_SHORT).show()
                 }
             }) {
                 Text("匯出密碼")
             }
+            when {
+                isExportVaultsConfirmExpanded -> {
+                    ExportVaultsConfirm(onExportVaultsConfirmExpandedChange, onDeleted = {
+                        if (vaultUiState.vaultList.isNotEmpty() || folderUiState.folderList.isNotEmpty()) {
+                            val timeStamp: String
+                            if (Build.VERSION.SDK_INT >= 26) {
+                                val formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
+                                timeStamp = LocalDateTime.now().format(formatter).toString()
+                            } else {
+                                val simpleDateFormat =
+                                    SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault())
+                                timeStamp = simpleDateFormat.format(Date())
+                            }
+                            val createFileIntent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+                                addCategory(Intent.CATEGORY_OPENABLE)
+                                type = "application/json"
+                                putExtra(Intent.EXTRA_TITLE, "vaultguard-$timeStamp-export.json")
+                            }
+                            viewModel.updateUiState(
+                                viewModel.exportVaultsUiState.exportVaults.copy(
+                                    password = ""
+                                )
+                            )
+                            createFileResultLauncher.launch(createFileIntent)
+                        } else {
+                            Toast.makeText(context, "尚未儲存密碼或資料夾!", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    })
+                }
+            }
         }
     }
+}
+
+@Composable
+fun ExportVaultsConfirm(
+    onExportVaultsConfirmExpandedChange: (Boolean) -> Unit,
+    onDeleted: () -> Unit,
+) {
+    AlertDialog(icon = {
+        Icon(Icons.Default.Warning, "警告")
+    }, title = {
+        Text(text = "是否要匯出密碼?")
+    }, text = {
+        Text(text = "匯出後的密碼將不會受到加密保護，請妥善保管您的密碼!")
+    }, onDismissRequest = {
+        onExportVaultsConfirmExpandedChange(false)
+    }, confirmButton = {
+        TextButton(onClick = {
+            onExportVaultsConfirmExpandedChange(false)
+        }) {
+            Text("取消")
+        }
+    }, dismissButton = {
+        TextButton(onClick = {
+            onDeleted()
+            onExportVaultsConfirmExpandedChange(false)
+        }) {
+            Text("確定")
+        }
+    })
 }
