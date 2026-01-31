bc174735388a94a8bd9b2373670d5a62adb8ebae
keke125 <admin@keke125.com>
2026-01-31 20:09:20 +0800

    chore: Release v1.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.keke125.vaultguard"
         minSdk = 24
         targetSdk = 35
-        versionCode = 32
-        versionName = "1.8.2"
+        versionCode = 33
+        versionName = "1.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
